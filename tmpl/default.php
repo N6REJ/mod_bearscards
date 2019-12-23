e69596357d0ecbb5fb316d8bce027fd6ba107243
Troy T. Hall <programming@hallhome.us>
2019-12-23 13:43:14 -0600

    version j.3.9.2019.12.23 all works well close #1 close #3

--- tmpl/default.php
+++ tmpl/default.php
@@ -9,36 +9,55 @@
  */
 
 defined('_JEXEC') or die;
+
 use Joomla\CMS\HTML\HTMLHelper;
-/** @var string $theme */
-/** @var string $customCSS */
+use Joomla\CMS\Factory;
+/** @var string $params */
 
 // load J! icon set
 HTMLHelper::_('stylesheet', 'mod_bearscards/default.css', array('version' => 'auto', 'relative' => true));
 // Used with HTMLHelper::
+/**
+ * ==================================================
+ * Joomla Variables
+ * ==================================================
+ */
+$app             = Factory::getApplication();
+$doc             = Factory::getDocument();
+
+$moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
+
 ?>
-<h4>Price Card</h4>
-<div class = "card card-price">
-	<div class = "card-img">
-		<a href = "#">
-			<img src = "http://placeimg.com/640/320/nature/grayscale" class = "img-responsive">
-			<div class = "card-caption">
-				<span class = "h2">Big Item</span>
-				<p>100% silk</p>
+<div class = "<?php echo $moduleclass_sfx; ?>">
+	<h4 class="card-header"><?php echo $params->get('header'); ?></h4>
+	<div class = "card card-price">
+		<div class = "card-img">
+			<?php if($params->get('image')){?>
+			<a href = "<?php echo $params->get('link'); ?>">
+				<img src = "<?php echo $params->get('image'); ?>" class = "img-responsive"
+				     alt = "<?php echo $params->get('imageAlt'); ?>">
+				<div class = "card-caption">
+					<span class = "<?php echo $params->get('captionSize'); ?>"><?php echo $params->get('caption'); ?></span>
+				</div>
+			</a>
+			<?php } ?>
+		</div>
+		<div class = "card-body">
+			<div class = "details">
+				<?php echo $params->get('body'); ?>
 			</div>
-		</a>
-	</div>
-	<div class = "card-body">
-		<div class = "price">$20 <small>each</small></div>
-		<div class = "lead">Wrap yourself in luxury</div>
-		<ul class = "details">
-			<li>A stitch in time saves nine.</li>
-			<li>All good things come to those who wait.</li>
-			<li><b>Shipping:</b> $10 in USA, $15 outside USA</li>
-		</ul>
-		<a href = "#" class = "btn btn-primary btn-lg btn-block buy-now">
-			Buy now <span class = "glyphicon glyphicon-triangle-right"></span>
-		</a>
+			<?php
+			if ( $params->get('button') )
+			{ ?>
+				<a href = "<?php echo $params->get('link'); ?>"
+				   class = "btn btn-<?php echo $params->get('buttonType'); ?> btn-lg btn-block buy-now">
+					<?php echo $params->get('buttonText'); ?>
+				</a>
+			<?php } ?>
+		</div>
+		<?php if ( $params->get('footer')){?>
+		<div class="card-footer"><?php echo $params->get('footer'); ?></div>
+		<?php } ?>
 	</div>
 </div>
 
