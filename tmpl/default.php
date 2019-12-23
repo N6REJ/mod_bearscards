ae40ef97fe3747278c52a3c71c2ebea941ad5b27
Troy T. Hall <programming@hallhome.us>
2019-12-23 10:31:42 -0600

    basic structure working.

--- tmpl/default.php
+++ tmpl/default.php
@@ -9,11 +9,38 @@
  */
 
 defined('_JEXEC') or die;
+use Joomla\CMS\HTML\HTMLHelper;
+/** @var string $theme */
+/** @var string $customCSS */
 
-// Access to module parameters
-$domain = $params->get('domain', 'https://www.joomla.org');
+$HTMLHelperDebug = array('version' => 'auto', 'relative' => true, 'detectDebug' => true);
+
+// load J! icon set
+HTMLHelper::_('stylesheet', 'css/default.css', $HTMLHelperDebug);
+// Used with HTMLHelper::
 ?>
+<h4>Price Card</h4>
+<div class = "card card-price">
+	<div class = "card-img">
+		<a href = "#">
+			<img src = "http://placeimg.com/640/320/nature/grayscale" class = "img-responsive">
+			<div class = "card-caption">
+				<span class = "h2">Big Item</span>
+				<p>100% silk</p>
+			</div>
+		</a>
+	</div>
+	<div class = "card-body">
+		<div class = "price">$20 <small>each</small></div>
+		<div class = "lead">Wrap yourself in luxury</div>
+		<ul class = "details">
+			<li>A stitch in time saves nine.</li>
+			<li>All good things come to those who wait.</li>
+			<li><b>Shipping:</b> $10 in USA, $15 outside USA</li>
+		</ul>
+		<a href = "#" class = "btn btn-primary btn-lg btn-block buy-now">
+			Buy now <span class = "glyphicon glyphicon-triangle-right"></span>
+		</a>
+	</div>
+</div>
 
-<a href="<?php echo $domain; ?>">
-	<?php echo 'mod_bearscards'; ?>
-</a>
