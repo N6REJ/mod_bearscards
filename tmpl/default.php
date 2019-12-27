3f31082693208095df3c87c525ba58f2646d05fe
Troy T. Hall <programming@hallhome.us>
2019-12-27 15:55:08 -0600

    added support for editor width with jce.

--- tmpl/default.php
+++ tmpl/default.php
@@ -12,6 +12,7 @@ defined('_JEXEC') or die;
 
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Factory;
+
 /** @var string $params */
 
 // Load CSS
@@ -22,41 +23,41 @@ HTMLHelper::_('stylesheet', 'mod_bearscards/default.css', array('version' => 'au
  * Joomla Variables
  * ==================================================
  */
-$app             = Factory::getApplication();
-$doc             = Factory::getDocument();
+$app = Factory::getApplication();
+$doc = Factory::getDocument();
 
 $moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
+var_dump($params);
 
 ?>
-<div class = "<?php echo $moduleclass_sfx; ?>">
+<div class="<?php echo $moduleclass_sfx; ?>">
 	<h4 class="card-header"><?php echo $params->get('header'); ?></h4>
-	<div class = "card card-price">
-		<div class = "card-img">
-			<?php if($params->get('image')){?>
-			<a href = "<?php echo $params->get('link'); ?>">
-				<img src = "<?php echo $params->get('image'); ?>" class = "img-responsive"
-				     alt = "<?php echo $params->get('imageAlt'); ?>">
-				<div class = "card-caption">
-					<span class = "<?php echo $params->get('captionSize'); ?>"><?php echo $params->get('caption'); ?></span>
-				</div>
-			</a>
+	<div class="card card-price">
+		<div class="card-img">
+			<?php if ( $params->get('image') ) { ?>
+				<a href="<?php echo $params->get('link'); ?>">
+					<img src="<?php echo $params->get('image'); ?>" class="img-responsive" alt="<?php echo $params->get('imageAlt'); ?>">
+					<div class="card-caption">
+						<span class="<?php echo $params->get('captionSize'); ?>"><?php echo $params->get('caption'); ?></span>
+					</div>
+				</a>
 			<?php } ?>
 		</div>
-		<div class = "card-body">
-			<div class = "details">
+		<div class="card-body">
+			<div class="details">
 				<?php echo $params->get('body'); ?>
 			</div>
 			<?php
 			if ( $params->get('button') )
 			{ ?>
-				<a href = "<?php echo $params->get('link'); ?>"
-				   class = "btn btn-<?php echo $params->get('buttonType'); ?> btn-lg btn-block buy-now">
+				<a href="<?php echo $params->get('link'); ?>"
+				   class="btn btn-<?php echo $params->get('buttonType'); ?> btn-lg btn-block buy-now">
 					<?php echo $params->get('buttonText'); ?>
 				</a>
 			<?php } ?>
 		</div>
-		<?php if ( $params->get('footer')){?>
-		<div class="card-footer"><?php echo $params->get('footer'); ?></div>
+		<?php if ( $params->get('footer') ) { ?>
+			<div class="card-footer"><?php echo $params->get('footer'); ?></div>
 		<?php } ?>
 	</div>
 </div>
